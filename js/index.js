5c262fcb8afb9c53296e8d9562c56a5362a1cbbf
Maurina Davis <Maurina.Davis@gmail.com>
2018-11-15 11:07:35 -0700

    landing page html added

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
 // Custom JS here
+const cardCreator = (spa) =>{
+ let card = document.querySelector('#cards')
+ card.className = "card"
+ card.addEventListener( 'hover', function() {
+     card.classList.toggle('is-flipped');
+ })
+}
