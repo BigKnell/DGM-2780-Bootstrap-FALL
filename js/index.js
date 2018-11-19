5017c3790e4e5700e5b2a15e40a044eaa3c2d5c1
Maurina Davis <Maurina.Davis@gmail.com>
2018-11-19 16:50:21 -0700

    Greensock added, working on navigation animation

--- js/index.js
+++ js/index.js
@@ -1,7 +1,34 @@
 // Custom JS here
-function toggleMenu() {
-    document.getElementById('hamburgerBtn').classList.toggle('open');
-    document.getElementById('primaryNav').classList.toggle('open');
-}
-let x = document.getElementById('hamburgerBtn');
-x.onclick = toggleMenu
+let t1 = new TimelineMax({paused: true});
+
+t1.to(".one", 0.8, {
+    y: 6,
+    rotation:45,
+    ease: Expo.easeInOut
+});
+
+t1.to(".two", 0.8, {
+    y: -6,
+    rotation: -45,
+    ease:Expo.easeInOut,
+    delay: -0.8
+});
+
+t1.to(".menu", 1,{
+    top: "0%",
+    ease: Expo.easeInOut,
+    delay: -1
+});
+
+
+t1.staggerFrom(".menu ul li", 2, {x: -200, opacity: 0, ease: Expo.easeOut}, 0.3);
+
+t1.reverse();
+$(document).on("click", ".toggle-btn", function(){
+    t1.reversed(!t1.reversed())
+});
+
+t1.reverse();
+$(document).on("click", "a", function(){
+    t1.reversed(!t1.reversed())
+});
