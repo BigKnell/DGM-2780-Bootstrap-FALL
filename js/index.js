0b12cf53d654cd0d2cfcd0a974916ceba39a0ded
Julie Halversen <juliannehalversen@gmail.com>
2018-12-06 12:57:56 -0700

    added gsap elements. logo spin, contact button, and tall image hover.

--- js/index.js
+++ js/index.js
@@ -1,3 +1,40 @@
 // Custom JS here
-$(x).button('toggle');
-var x = document.getElementById("contactbutton");
+let thumb1 = $("#thumb1");
+let thumb2 = $("#thumb2");
+let thumb3 = $("#thumb3");
+let thumb4 = $("#thumb4");
+
+thumb1.mouseenter(function() {
+    var tween = TweenMax.from(thumb1, 1, {
+        scale: 1.20,
+    });
+});
+
+thumb2.mouseenter(function() {
+    var tween = TweenMax.from(thumb2, 1, {
+        scale: 1.20,
+    });
+});
+
+thumb3.mouseenter(function() {
+    var tween = TweenMax.from(thumb3, 1, {
+        scale: 1.20,
+    });
+});
+
+thumb4.mouseenter(function() {
+    var tween = TweenMax.from(thumb4, 1, {
+        scale: 1.20,
+    });
+});
+
+
+
+
+TweenMax.to("#logo", 1, {rotationX: 360});
+
+const animateBtn = document.querySelector('#animate-btn');
+
+animateBtn.addEventListener('click', function(){
+    TweenMax.to("#form", 1, {x:100});
+});
