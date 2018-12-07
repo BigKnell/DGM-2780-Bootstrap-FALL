8b8d70a04a4e378417b5950322871f9c509c2115
Krystal Mitchell <10262399@my.uvu.edu>
2018-12-06 19:44:06 -0700

    GreenSock Logo and form button animation

--- js/index.js
+++ js/index.js
@@ -1,1 +1,23 @@
-// Custom JS here
+/*external js
+https://cdnjs.cloudflare.com/ajax/libs/gsap/latest/TweenLite.min.js
+https://cdnjs.cloudflare.com/ajax/libs/gsap/latest/plugins/CSSPlugin.min.js
+*/
+
+window.onload = function() {
+  var logo = document.getElementById("logo");
+  TweenLite.to(logo, 2, {left:"80px", backgroundColor:"none", borderBottomColor:"#90e500", color:"white"});
+}
+
+//basic illustration of TweenMax's repeat, repeatDelay, yoyo and onRepeat
+var box = document.getElementById("greenBox"),
+    count = 0,
+    tween;
+
+tween = TweenMax.to(box, 2, {left:"700px", repeat:5, yoyo:true, onRepeat:onRepeat, repeatDelay:0.5, ease:Linear.easeNone});
+
+function onRepeat() {
+  count++;
+  box.innerHTML = count;
+  TweenLite.set(box, {backgroundColor:"hsl(" + Math.random() * 255 + ", 90%, 60%)"});
+}							
+						
