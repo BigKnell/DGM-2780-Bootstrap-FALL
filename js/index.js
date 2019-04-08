d2f2ac3dedea86606b091afd0ebe486328a224e9
Riley Giles <huzzh>
2019-04-07 23:12:37 -0600

    Finished bootstrap website.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,28 @@
-// Custom JS here
+$(document).ready(function(){
+
+    //Carousel
+    $("#javaCarousel").carousel();
+    $(".pic1").click(function() {
+        $("#javaCarousel").carousel(0);
+    });
+    $(".pic2").click(function() {
+        $("#javaCarousel").carousel(1);
+    });
+    $(".pic3").click(function() {
+        $("#javaCarousel").carousel(2);
+    }); 
+    $(".pic4").click(function() {
+        $("#javaCarousel").carousel(3);
+    });
+    $(".carousel-control-prev").click(function(){
+        $("#javaCarousel").carousel("prev");
+    });
+    $(".carousel-control-next").click(function(){
+        $("#javaCarousel").carousel("next");
+    });
+
+    //Collapsable alert
+    $(".closer").click(function() {
+        $("#formAlert").alert("close");
+    });
+});
