2790b172d89c94e3d313e867ed2d9f6ebef5f8d7
Riley Giles <huzzh>
2019-04-23 19:39:14 -0600

    Shaking badges and running logos.

--- js/index.js
+++ js/index.js
@@ -25,4 +25,27 @@ $(document).ready(function(){
     $(".closer").click(function() {
         $("#formAlert").alert("close");
     });
+
+    //GreenSock
+
+    //This one will send the logo to the other side of the screen
+    //if you mouse over it.
+    $(".logo").mouseover(function() {
+        TweenLite.to(".logo", 4, {ease: Power3.easeIn, x: "60%"});
+    });
+
+    //Makes the badges shake when the mouse leaves.
+    $(".badge").each(function(){
+        $(this).mouseleave(function() {
+            TweenLite.to(this, 3, {ease: RoughEase.ease.config({
+                template: Sine.easeIn,
+                strength: 1,
+                points: 30,
+                taper:"none",
+                randomize: false,
+                clamp: false
+            }),
+        y: 5});
+        });
+    })
 });
